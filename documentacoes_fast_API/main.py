0c65566530cd4887c3eb9ff6c04d82172ed10911
iscaglioni-target <ivan.scaglioni@targetdata.com.br>
2025-04-11 16:20:41 -0300

    create: "classe genéria erros"

--- documentacoes_fast_API/main.py
+++ documentacoes_fast_API/main.py
@@ -10,53 +10,64 @@ class UserResponse(BaseModel):
 class ErrorResponse(BaseModel):
     detail: str
 
+codigos_erro = [
+    {"status": 400, "mensagem": "O ID de usuário fornecido não é válido."},
+    {"status": 404, "mensagem": "Usuário não encontrado"},
+    {"status": 500, "mensagem": "Erro interno do servidor"}
+    ]
+
+class Examples_erros:
+    def __init__(self, status: int, description: str):
+        self.status = status
+        self.description = description
+        
+
+    def montar_erro(self):
+        return {
+            self.status : {
+                "model": ErrorResponse,
+                "description": self.description,
+                "content": {
+                    "application/json": {
+                    "example": {
+                    "detail": self.description
+                    }
+                }
+                }
+
+            }
+
+        }    
+    
+erro400 = Examples_erros(status=codigos_erro[0]["status"], description=codigos_erro[0]["mensagem"])
+erro404 = Examples_erros(status=codigos_erro[1]["status"], description=codigos_erro[1]["mensagem"])
+erro500 = Examples_erros(status=codigos_erro[2]["status"], description=codigos_erro[2]["mensagem"])
+
+responses = {}
+responses.update(erro404.montar_erro())
+responses.update(erro400.montar_erro())
+responses.update(erro500.montar_erro())
+
+usuarios = [
+    {"id": 1, "nome": "Ivan"},
+    {"id": 2, "nome": "Rafael"},
+    {"id": 1, "nome": "Sávio"},
+]
+
 @app.get(
     "/users/{user_id}",
     response_model=UserResponse,
-    responses={
-        400: {
-            "model": ErrorResponse,
-            "description": "ID de usuário inválido",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "O ID de usuário fornecido não é válido."
-                    }
-                }
-            }
-        },
-        404: {
-            "model": ErrorResponse,
-            "description": "Usuário não encontrado",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Usuário não encontrado."
-                    }
-                }
-            }
-        },
-        500: {
-            "model": ErrorResponse,
-            "description": "Erro interno do servidor",
-            "content": {
-                "application/json": {
-                    "example": {
-                        "detail": "Erro inesperado. Tente novamente mais tarde."
-                    }
-                }
-            }
-        }
-    }
+    responses= responses
 )
 async def get_user(user_id: str):
     if not user_id.isdigit():
-        raise HTTPException(status_code=400, detail="O ID de usuário fornecido não é válido.")
+        raise HTTPException(status_code=codigos_erro[0]["status"], detail=codigos_erro[0]["mensagem"])
     
-    if user_id != "1234":
-        raise HTTPException(status_code=404, detail="Usuário não encontrado.")
+    for id in user_id:
+        if user_id != id:
+            raise HTTPException(status_code=codigos_erro[1]["status"], detail=codigos_erro[1]["mensagem"])
     
     try:
         return {"user_id": user_id, "name": "Ivan"}
     except Exception:
-        raise HTTPException(status_code=500, detail="Erro inesperado. Tente novamente mais tarde.")
+        raise HTTPException(status_code=codigos_erro[2], detail=codigos_erro["mensagem"])
